9240bf969ecf1281a1770464d2f97988a6f0e381
tarpeyd12 <dean.tarpey@gmail.com>
2019-05-01 15:44:00 -0700

    cleaned up some windows only stuff.
    macrod out setting process priority to low and lowest because it uses
    windows only API calls aht the moment.

--- spnn/tests/test_helpers.cpp
+++ spnn/tests/test_helpers.cpp
@@ -1,4 +1,6 @@
+#if defined( _WIN32 )
 #include <windows.h>
+#endif
 
 #include "tests.hpp"
 
@@ -7,12 +9,16 @@ namespace _tests
     void
     SetProcessPriority_low()
     {
+        #if defined( _WIN32 )
         SetPriorityClass( GetCurrentProcess(), BELOW_NORMAL_PRIORITY_CLASS );
+        #endif
     }
 
     void
     SetProcessPriority_lowest()
     {
+        #if defined( _WIN32 )
         SetPriorityClass( GetCurrentProcess(), IDLE_PRIORITY_CLASS );
+        #endif
     }
 }
