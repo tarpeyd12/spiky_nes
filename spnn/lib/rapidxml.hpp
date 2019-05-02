df8e106a82ec03809027833946f1f0eb8c9c508f
tarpeyd12 <dean.tarpey@gmail.com>
2019-05-01 21:08:53 -0700

    added rapidxml and base64.cpp
    http://rapidxml.sourceforge.net/
    
    https://renenyffenegger.ch/notes/development/Base64/Encoding-and-decoding-base-64-with-cpp
    https://github.com/ReneNyffenegger/cpp-base64

---
+++ spnn/lib/rapidxml.hpp
@@ -0,0 +1,6 @@
+#ifndef SPNN_RAPIDXML_HPP_INCLUDED
+#define SPNN_RAPIDXML_HPP_INCLUDED
+
+#include "rapidxml/rapidxml.hpp"
+
+#endif // SPNN_RAPIDXML_HPP_INCLUDED
---
+++ spnn/neat/xml.hpp
@@ -0,0 +1,13 @@
+#ifndef XML_HPP_INCLUDED
+#define XML_HPP_INCLUDED
+
+#include "../lib/rapidxml.hpp"
+
+#include "neat.hpp"
+
+namespace neat
+{
+
+}
+
+#endif // XML_HPP_INCLUDED
